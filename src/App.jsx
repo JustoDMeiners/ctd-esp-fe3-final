5cc97bbfaee8125f99708171854993157155dc19
JustoDMeiners <justito_dm@hotmail.com>
2022-12-01 18:06:51 -0300

    Push

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 
+import { useContext } from "react";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 
 
+
+const light = useContext(light)
+
+
 function App() {
   return (
-      <div className="App">
-          <Navbar/>
-          <Footer/>
+      <div className="light">
+        <div className="App" >
+            <Navbar/>
+            <Footer/>
+        </div>
       </div>
   );
 }
